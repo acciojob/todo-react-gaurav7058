ed56af3c1d619daeb49083e36b05fad8a5ffb2d1
Create_first_repo <gauravchoundiye123@gmail.com>
2024-09-16 17:28:38 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todo, setTodo] = useState([]);
+  const [taskList,setTaskList]=useState("");
+  const [showData,setShowData]=useState(true);
+  
+  function todoFun(e){
+    e.preventDefault();
+    if(taskList.trim()){
+      setTodo([...todo,taskList]);
+    }
+  }
+  const deleteTodo = (index) => {
+    const newTodos = todo.filter((_, i) => i !== index);
+    setTodo(newTodos);
+  };
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="">
+          <form action="" onSubmit={todoFun}>
+            <input type="text" value={taskList} onChange={(e)=>setTaskList(e.target.value)}/>
+            <button type="submit">Add Todo</button>
+          </form>
+        </div>
+        <ul>
+        {todo.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 export default App
+
